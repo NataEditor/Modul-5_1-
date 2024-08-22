56219733c1908ec82f97c1006e4306614735791b
NataEditor <nako_al@mail.ru>
2024-08-22 08:02:28 +0300

    Update modul_5_1 ispr.py

--- modul_5_1.py
+++ modul_5_1.py
@@ -1,24 +1,25 @@
 
 class House:
-    def __init__(self,name,namber_of_floors):
+    def __init__(self,name,number_of_floors):
         self.name=name
-        self.namber_of_floors=namber_of_floors
+        self.number_of_floors=number_of_floors
         
     def go_to(self,new_floor):
         i=0
-        if new_floor>self.namber_of_floors or new_floor<1:
+        if new_floor>self.number_of_floors or new_floor<1:
             print('takogo etaga net')
-            exit()
-        while new_floor:
-            i+=1
-            print(i)
-            if i>=new_floor:
-                exit()
+            
+        else:
+            for i in range(new_floor):
+                i+=1
+                print(i)
+            
                 
 
     
 dev=House('GK Les', 12)
-print(dev.name, dev.namber_of_floors)
+print(dev.name, dev.number_of_floors)
 dev2=House('Domik v derevne', 2)
-print(dev2.name, dev2.namber_of_floors)
+print(dev2.name, dev2.number_of_floors)
 dev2.go_to(2)
+
